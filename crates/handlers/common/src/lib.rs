4d75ca90cb2ba268af6647e994ae5b238a243fb4
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2025-04-22 01:17:11 +0530

    lint fixes

--- crates/handlers/common/src/lib.rs
+++ crates/handlers/common/src/lib.rs
@@ -1,7 +1,7 @@
-pub mod get_chain_id;
 pub mod declare_program;
 pub mod deploy_account;
-pub mod get_program;
+pub mod get_chain_id;
 pub mod get_nonce;
-pub mod send_transaction;
+pub mod get_program;
 pub mod get_transaction_receipt;
+pub mod send_transaction;
--- crates/handlers/starknet/src/tests/mod.rs
+++ crates/handlers/starknet/src/tests/mod.rs
@@ -1,19 +1,19 @@
 #[cfg(test)]
-mod get_chain_id;
-#[cfg(test)]
 mod declare_program;
 #[cfg(test)]
 mod deploy_account;
 #[cfg(test)]
-mod get_program;
+mod get_chain_id;
 #[cfg(test)]
 mod get_nonce;
+#[cfg(test)]
+mod get_program;
+#[cfg(test)]
+mod get_transaction_receipt;
 #[cfg(test)]
 mod read_data;
 #[cfg(test)]
 mod send_transaction;
-#[cfg(test)]
-mod get_transaction_receipt;
 
 #[cfg(any(test, feature = "testing"))]
 pub mod utils;
