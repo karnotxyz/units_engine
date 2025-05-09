ae7e1c971a5a168f4920911ddb8d1df1142a594c
Mehul Chauhan <imehul24@gmail.com>
2025-05-09 20:23:56 +0530

    add claim fun

--- js-scripts/src/scripts/compliance/add_claim.ts
+++ js-scripts/src/scripts/compliance/add_claim.ts
@@ -1,12 +1,13 @@
 import assert from "assert";
 import { UnitsAccount } from "../../account";
 import { UnitsProvider } from "../../provider";
+import {num} from "starknet";
 import dotenv from "dotenv";
 import { sleep } from "./utils";
 
 dotenv.config();
 
-async function add_claim(token: string, topic: string) {
+async function add_claim(user: string, topic: string) {
   console.log(process.env.UNITS_RPC);
   const unitsProvider = new UnitsProvider(process.env.UNITS_RPC);
   const unitsAccount = new UnitsAccount(
@@ -15,18 +16,21 @@ async function add_claim(token: string, topic: string) {
     process.env.PRIVATE_KEY,
   );
 
+  let { result } = await unitsAccount.call(process.env.IDENTITY_REGISTRY, "get_identity", [user]);
+  console.log("oid of user", result[0]);
+
   let { transaction_hash } = await unitsAccount.sendTransaction(
     [
       {
-        contractAddress: token,
+        contractAddress: result[0],
         entrypoint: "add_claim",
         calldata: [topic]
       }
     ]
   );
 
-  await sleep(5000);
-  console.log("✅ Initiated adding claim check:", transaction_hash);
+  await sleep(2000);
+  console.log("✅ Initiated adding claim:", transaction_hash);
 
   const receipt = await unitsAccount.getTransactionReceipt(transaction_hash);
   assert(receipt.execution_status.type == "SUCCEEDED")
@@ -34,12 +38,12 @@ async function add_claim(token: string, topic: string) {
 
 /// CLI HELPERS
 
-if (process.argv.length < 3) {
-  console.error("Usage: ts-node add_claim.ts <token> <topic>");
+if (process.argv.length < 4) {
+  console.error("Usage: ts-node add_claim.ts <user> <topic>");
   process.exit(1);
 }
 
-const token = process.argv[2];
+const user = process.argv[2];
 const topic = process.argv[3];
 
-add_claim(token, topic);
+add_claim(user, topic);
